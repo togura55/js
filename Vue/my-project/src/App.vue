116342f83d48a90cb283c931ffb1b3fc0a07e5da
Tsuyoshi <togura55@gmail.com>
2021-01-18 14:52:25 +0900

    add comments
    add comments

--- Vue/my-project/src/App.vue
+++ Vue/my-project/src/App.vue
@@ -1,3 +1,8 @@
+<!--
+index.html -> main.js -> App.vue -> 子コンポーネント.vue の順に入れ子
+-->
+
+<!-- HTMLの記述-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
@@ -17,28 +22,32 @@
   </div>
 </template>
 
+<!-- スクリプトの記述 -->
 <script>
 // 呼び出したい子コンポーネントファイルのimport。パスを通す
 // キー名と変数名が同じ場合は省略する構文が使える（非省略 Counter: Counter)
 import HelloWorld from './components/HelloWorld.vue'
 import Counter from './components/Counter.vue'
 
-//
+// 単一ファイルコンポーネントで、scriptを外部のコンポーネントからも
+// 参照できるように宣言
 export default {
-  //
+  // このコンポーネントの名前を定義
   name: 'App',
   // 子コンポーネントをマウント
   components: {
     HelloWorld,
     Counter
   },
-  //
+  // コンポーネント内でデータや状態を保持したい場合に使用
+  // データ名と初期値のペアの連想配列をreturn
+  //   <データ名>: <初期値>,
   data(){
     return{
       stack: []
     }
   },
-  //
+  // コンポーネント内のtemplateやscriptで使用するメソッドの定義
   methods: {
     getEvent(payload){
       this.stack.push(payload)
@@ -47,6 +56,7 @@ export default {
 }
 </script>
 
+<!-- CSSの記述 -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- Vue/my-project/src/main.js
+++ Vue/my-project/src/main.js
@@ -1,8 +1,17 @@
+// index.html -> main.js -> App.vue -> 子コンポーネント.vue の順に入れ子
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Vueインスタンス作成
+//
+// el: '#app'ではなく .$mount ('#app')を使用すると、Vueインスタンスを明示的にマウントする。 
+// これは、特定の要素がページに存在するか、非同期プロセスが完了するまで、Vueインスタンスの
+// マウントを遅らせることができる。
+// これは、要素をDOMに挿入するレガシーアプリにVueを追加するときに特に役立ちます。 
+// これは、複数のテストで同じvueインスタンスを使用したい場合のテストで頻繁に発生する。
 new Vue({
-  render: h => h(App),
+  render: h => h(App), // templateレンダリングの代替...黙って記述
 }).$mount('#app')
