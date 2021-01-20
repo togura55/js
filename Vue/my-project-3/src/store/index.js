0710445fb2648d43c30a596bdbd583786f772efc
Tsuyoshi <togura55@gmail.com>
2021-01-20 10:36:11 +0900

    #3
    updated

--- Vue/my-project-3/src/store/index.js
+++ Vue/my-project-3/src/store/index.js
@@ -1,30 +1,53 @@
 //
 // Vuexテンプレート
 //
+//import { Store } from 'node_modules/vuex/types/index'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // Vuexのstateは....
   // グローバルな状態を保持するプロパティ
   state: {
-    globalCount: 0
+    globalCount: 0,
+    profile: null,
   },
 
+  // Vuexのmutationsはストアへのコミット目的で使用
   // stateプロパティへの書き込みは
-  // mutationsの中でしか行わない
+  // ミューテーションの中でしか行わない
   mutations: {
-    // mutationsで定義されたメソッドは
+    // mutationsで定義されたハンドラーメソッド
     // 第一引数に必ずstateを持つこと
     globalIncrement(state){
       state.globalCount++ // stateへの書き込み
-    }
+    },
+
+    setGithubProfile(state, payload){
+      state.profile = payload;
+    },
   },
 
-
+  // Vuexのactionsはディスパッチ目的で使用
+   // 任意の非同期処理を含むことができる
   actions: {
+    // state.profileに値があれば早期にリターン。
+    // 無ければAPIコールし、得られた値をミューテーションでコミットさせる
+    async fetchGithubProfile(store, payload){
+      if (store.state.profile != null) return;
+
+      const response = await fetch(
+        `https://api.github.com/users/${payload.user_id}`
+      ).then((res) => {
+        return res.json();
+      });
+      store.commit("setGithubProfile", response);
+    },
   },
+  
+  // Vuexのmodulesは....
   modules: {
   }
-})
+});
--- Vue/my-project-3/src/views/About.vue
+++ Vue/my-project-3/src/views/About.vue
@@ -1,5 +1,24 @@
+<!--
+
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ profile }}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  // mountedライフサイクルを使って呼び出される
+  mounted() {
+    this.$store.dispatch("fetchGithubProfile", 
+    { user_id: "togura55" })
+  },
+
+  computed: {
+    ...mapState(["profile"]),
+  },
+};
+</script>
