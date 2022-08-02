a760abfd275762f2777b69fa103a916ba855d2f2
Tsuyoshi Ogura <togura55@gmail.com>
2022-08-02 13:17:23 +0900

    updated

--- HtmlControls/index.js
+++ HtmlControls/index.js
@@ -1,98 +1,118 @@
-let penUp = 0;
-var prev_X, prev_Y;
-var zArr = new Array();
-var zMax = 0, zMin = 1;
+// let penUp = 0;
+// var prev_X, prev_Y;
+// var zArr = new Array();
+// var zMax = 0, zMin = 1;
 
 window.addEventListener('load', () => {
+    function onRadioButtonChange() {
+        check1 = document.formRadioButton.radioButtonPng.checked;
+        check2 = document.formRadioButton.radioButtonJpg.checked;
 
-    const mainCanvas = document.getElementById('mainCanvas')
+        target = document.getElementById("output");
 
-    mainCanvas.width = 500
-    mainCanvas.height = 500
-
-    const context = mainCanvas.getContext('2d')
-
-    function log(tag, e) {
-        console.log(tag, e.pointerId, e.offsetX, e.offsetY, e.buttons, e.pressure)
-    }
-
-    var elData = document.getElementById("pointerData");
-    var elZMaxMin = document.getElementById("zMaxMin");
-
-    function drawPointer(e, p) {
-        const x = e.offsetX
-        const y = e.offsetY
-        const z = e.pressure
-        const radius = 5.0 + e.pressure * 10.0
-        const red = e.pressure * 255
-        context.beginPath()
-        context.moveTo((x + radius), y)
-        context.arc(x, y, radius, 0, Math.PI * 2)
-        context.closePath()
-        context.fill()
-
-        if (e.buttons != 0 || e.pressure > 0.0) {
-            var color = `rgba(${red}, 0, 255, 1.0)`;
-            context.fillStyle = color;
-
-            // If a pen is down, draw line
-            if (penUp == 0 && p == 0) {
-                // Reset the current path
-                context.beginPath();
-                context.lineWidth = radius * 2;
-                context.strokeStyle = color;
-                context.moveTo(x, y);
-                context.lineTo(prev_X, prev_Y);
-                // Make the line visible
-                context.stroke();
-
-                if (zMax < z) zMax = z;
-                if (zMin > z) zMin = z;
-                elData.innerHTML = 'x=' + x + ', y=' + y + ', z=' + z;
-                elZMaxMin.innerHTML = 'zMax=' + zMax + ', zMin=' + zMin;
-                zArr.push(z);
-            }
-            prev_X = x;
-            prev_Y = y;
+        if (check1 == true) {
+            target.innerHTML = "要素1がチェックされています。<br/>";
         }
-        // in case of drawing the Pen Hover
-        else if (e.buttons == 0 && e.pressure == 0.0) {
-            context.fillStyle = `rgba(200, 200, 200, 1.0)`
-
-            elData.innerHTML = 'x=' + x + ', y=' + y + ', z=' + z + ', Hover';
+        else if (check2 == true) {
+            target.innerHTML = "要素2がチェックされています。<br/>";
         }
     }
 
-    mainCanvas.addEventListener('pointerdown', (e) => {
-        log('pointerdown', e)
-        drawPointer(e, 1)
-        penUp = 0;
-        e.preventDefault()
-    })
-
-    mainCanvas.addEventListener('pointermove', (e) => {
-        log('pointermove', e)
-        drawPointer(e, 0)
-        penUp = 0;
-        e.preventDefault()
-    })
-
-    mainCanvas.addEventListener('pointerup', (e) => {
-        log('pointerup', e)
-        penUp = 1;
-        e.preventDefault()
-    })
+    function onClickConfirm() {
+        alert('hello!')
+    }
 })
 
-document.getElementById('buttonExport').onclick = function(){
-	exportTextFile(zArr,'z.txt');
-}
+// window.addEventListener('load', () => {
 
-function exportTextFile(data, filename) {
-    let blob = new Blob([data], { type: "text/plan" }); // テキスト形式でBlob定義
-    let link = document.createElement('a'); // HTMLのaタグを作成
-    link.href = URL.createObjectURL(blob); // aタグのhref属性を作成
-    link.download = filename; // aタグのdownload属性を作成
-    link.click(); // 定義したaタグをクリック（実行）
-};
+//     const mainCanvas = document.getElementById('mainCanvas')
+
+//     mainCanvas.width = 500
+//     mainCanvas.height = 500
+
+//     const context = mainCanvas.getContext('2d')
+
+//     function log(tag, e) {
+//         console.log(tag, e.pointerId, e.offsetX, e.offsetY, e.buttons, e.pressure)
+//     }
+
+//     var elData = document.getElementById("pointerData");
+//     var elZMaxMin = document.getElementById("zMaxMin");
+
+//     function drawPointer(e, p) {
+//         const x = e.offsetX
+//         const y = e.offsetY
+//         const z = e.pressure
+//         const radius = 5.0 + e.pressure * 10.0
+//         const red = e.pressure * 255
+//         context.beginPath()
+//         context.moveTo((x + radius), y)
+//         context.arc(x, y, radius, 0, Math.PI * 2)
+//         context.closePath()
+//         context.fill()
+
+//         if (e.buttons != 0 || e.pressure > 0.0) {
+//             var color = `rgba(${red}, 0, 255, 1.0)`;
+//             context.fillStyle = color;
+
+//             // If a pen is down, draw line
+//             if (penUp == 0 && p == 0) {
+//                 // Reset the current path
+//                 context.beginPath();
+//                 context.lineWidth = radius * 2;
+//                 context.strokeStyle = color;
+//                 context.moveTo(x, y);
+//                 context.lineTo(prev_X, prev_Y);
+//                 // Make the line visible
+//                 context.stroke();
+
+//                 if (zMax < z) zMax = z;
+//                 if (zMin > z) zMin = z;
+//                 elData.innerHTML = 'x=' + x + ', y=' + y + ', z=' + z;
+//                 elZMaxMin.innerHTML = 'zMax=' + zMax + ', zMin=' + zMin;
+//                 zArr.push(z);
+//             }
+//             prev_X = x;
+//             prev_Y = y;
+//         }
+//         // in case of drawing the Pen Hover
+//         else if (e.buttons == 0 && e.pressure == 0.0) {
+//             context.fillStyle = `rgba(200, 200, 200, 1.0)`
+
+//             elData.innerHTML = 'x=' + x + ', y=' + y + ', z=' + z + ', Hover';
+//         }
+//     }
+
+//     mainCanvas.addEventListener('pointerdown', (e) => {
+//         log('pointerdown', e)
+//         drawPointer(e, 1)
+//         penUp = 0;
+//         e.preventDefault()
+//     })
+
+//     mainCanvas.addEventListener('pointermove', (e) => {
+//         log('pointermove', e)
+//         drawPointer(e, 0)
+//         penUp = 0;
+//         e.preventDefault()
+//     })
+
+//     mainCanvas.addEventListener('pointerup', (e) => {
+//         log('pointerup', e)
+//         penUp = 1;
+//         e.preventDefault()
+//     })
+// })
+
+// document.getElementById('buttonExport').onclick = function(){
+// 	exportTextFile(zArr,'z.txt');
+// }
+
+// function exportTextFile(data, filename) {
+//     let blob = new Blob([data], { type: "text/plan" }); // テキスト形式でBlob定義
+//     let link = document.createElement('a'); // HTMLのaタグを作成
+//     link.href = URL.createObjectURL(blob); // aタグのhref属性を作成
+//     link.download = filename; // aタグのdownload属性を作成
+//     link.click(); // 定義したaタグをクリック（実行）
+// };
 
